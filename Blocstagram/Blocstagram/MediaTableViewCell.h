87cbc7070582677b589a7996f5a34e50309578a3
Thanhduy Tran <thanhdut@usc.edu>
2015-06-19 11:19:26 -0500

    Checkpoint 29 complete

---
+++ Blocstagram/Blocstagram/MediaTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  MediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Thanhduy Tran on 6/18/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MediaTableViewCell : UITableViewCell
+
+@end
---
+++ Blocstagram/Blocstagram/MediaTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MediaTableViewCell.m
+//  Blocstagram
+//
+//  Created by Thanhduy Tran on 6/18/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import "MediaTableViewCell.h"
+
+@implementation MediaTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
