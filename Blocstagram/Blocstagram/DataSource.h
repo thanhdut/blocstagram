ed5285068adc0207d6b906bf7766e92b5788d19f
Thanhduy Tran <thanhdut@usc.edu>
2015-06-28 22:42:26 -0500

    Checkpoint 33 complete

--- Blocstagram/Blocstagram/DataSource.h
+++ Blocstagram/Blocstagram/DataSource.h
@@ -14,9 +14,11 @@ typedef void (^NewItemCompletionBlock)(NSError *error);
 
 @interface DataSource : NSObject
 
-+(instancetype) sharedInstance;
++ (instancetype) sharedInstance;
++ (NSString *) instagramClientID;
 
 @property (nonatomic, strong, readonly) NSArray *mediaItems;
+@property (nonatomic, strong, readonly) NSString *accessToken;
 
 - (void) deleteMediaItem:(Media *)item;
 - (void) requestNewItemsWithCompletionHandler:(NewItemCompletionBlock)completionHandler;
---
+++ Blocstagram/Blocstagram/LoginViewController.h
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.h
+//  Blocstagram
+//
+//  Created by Thanhduy Tran on 6/28/15.
+//  Copyright (c) 2015 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+extern NSString *const LoginViewControllerDidGetAccessTokenNotification;
+
+
+@end
